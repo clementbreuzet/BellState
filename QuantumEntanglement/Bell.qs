33bc9365ac7c66c7008436753466777f5628c36b
Clement Breuzet <clement.breuzet@avanade.com>
2020-02-28 23:04:51 +0100

    Add project files.

---
+++ QuantumEntanglement/Bell.qs
@@ -0,0 +1,40 @@
+﻿namespace Quantum.QuantumEntanglement {
+
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Intrinsic;
+    
+    operation TestBellState((q0Init: Result, q1Init: Result), iteration: Int, entangled: Bool) : (Int, Int) {
+        mutable onesMatch = 0;
+        mutable zerosMatch = 0;
+        using ((q0, q1) = (Qubit(), Qubit())) {
+            for (test in 1..iteration) {
+                Set(q0Init, q0);
+                Set(q1Init, q1);
+
+                H(q0);
+                if(entangled) {
+                    CNOT(q0, q1);
+				}
+                let q0result = M(q0);
+                let q1result = M(q1);
+                if (q0result == q1result) {
+                    if(q0result == One){
+                        set onesMatch += 1;           
+					} else{
+                        set zerosMatch += 1; 
+					}
+                }
+            }
+            
+            Set(Zero, q0);
+            Set(Zero, q1);
+        }
+        return (zerosMatch, onesMatch);
+	}
+
+    operation Set(desiredResult : Result, qubit : Qubit) : Unit {
+        if (desiredResult != M(qubit)) {
+            X(qubit);
+        }
+    }
+}
---
+++ QuantumEntanglement/DriverHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace QuantumEntanglement
+{
+	public static class DriverHelper
+	{
+		public static void WriteResult(long zerosMatch, long onesMatch, int iteration)
+		{
+			(string matchesString, string unmatchesString) = GetStrings(zerosMatch, onesMatch, iteration);
+			double matchRatio = GetMatchRatio(zerosMatch, onesMatch, iteration);
+			Console.Write("[ ");
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.Write($"{matchesString}");
+			Console.BackgroundColor = ConsoleColor.Red;
+			Console.Write($"{unmatchesString}");
+			Console.ResetColor();
+			Console.Write($" ] Ratio: {matchRatio}%");
+			Console.Write($" Zero: {zerosMatch} One: {onesMatch}");
+			Console.WriteLine(Environment.NewLine);
+		}
+
+		private static (string matchesString, string unmatchesString) GetStrings(long zerosMatch, long onesMatch, int iteration)
+		{
+			string matchesString = string.Empty;
+			string unmatchesString = string.Empty;
+			int matchRatio = Convert.ToInt32(Math.Round((zerosMatch + onesMatch) * 10.0) / iteration);
+			matchesString = matchesString.PadLeft(matchRatio);
+			unmatchesString = unmatchesString.PadLeft(10 - matchRatio);
+			return (matchesString, unmatchesString);
+		}
+
+		private static double GetMatchRatio(long zerosMatch, long onesMatch, int iteration)
+		{
+			return Math.Round((Convert.ToDouble(zerosMatch) + Convert.ToDouble(onesMatch)) / Convert.ToDouble(iteration) * 100.00, 4);
+		}
+	}
+}
